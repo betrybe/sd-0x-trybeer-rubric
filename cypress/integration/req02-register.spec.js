387c0dc92f9fe7ff57dab7f7e4fc4f67df3cb717
Renato Filho <renatosousafilho@gmail.com>
2021-04-29 20:23:33 -0300

    corrigindo linter

--- cypress/integration/req02-register.spec.js
+++ cypress/integration/req02-register.spec.js
@@ -37,7 +37,7 @@ describe('2 - Crie uma página de registro de usuários', () => {
     verifyElementVisible('[data-testid="signup-email"]');
     verifyContainsText('Senha');
     verifyElementVisible('[data-testid="signup-password"]');
-    verifyContainsText('Quero Vender');
+    verifyContainsText('Quero vender');
     verifyElementVisible('[data-testid="signup-seller"]');
     verifyContainsText('Cadastrar');
     verifyElementVisible('[data-testid="signup-btn"]');
